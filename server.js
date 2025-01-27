07cd9cdc868a07038d5f7678868ed78897f08ebf
MattyHup6434 <122677300+MattyHup6434@users.noreply.github.com>
2025-01-27 10:40:41 +0700

    2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 
 const PORT = process.env.PORT || 8080
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/', (req, res) => res.send('Hello World 2'))
 
 app.listen(PORT, () => {
 
